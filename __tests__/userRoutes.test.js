ab565c759fe9425eae824240ddd64ca383cb2d11
Arthur Hauser <arthur.hauser@gmail.com>
2024-07-12 14:06:09 -0400

    add comments to tests

--- __tests__/userRoutes.test.js
+++ __tests__/userRoutes.test.js
@@ -2,20 +2,24 @@ const request = require('supertest');
 const app = require('../src/app');
 const db = require('../src/fakeDb/db');
 
+//create a new user before each test.  This way we can test the GET, POST, PATCH, and DELETE routes.  We can also test the GET /users/:id route.  We can also test the GET /users route.  
 let testUser;
 beforeEach(async () => {
     const result = await db.query(`INSERT INTO users (name, type) VALUES ('Peanut', 'admin') RETURNING  id, name, type`);
     testUser = result.rows[0]
 })
 
+//delete any data created by test.  This way we can test the DELETE /users/:id route.  
 afterEach(async () => {
     await db.query(`DELETE FROM users`)
 })
 
+//close the database connection.  This way we can test the GET /users route.  
 afterAll(async () => {
     await db.end()
 })
 
+//test the GET /users route.  This route should return a list of all users that was created by the beforeEach function which in this case is one user with a 200 status code.  
 describe("GET /users", () => {
     test("Get a list with one user", async () => {
         const res = await request(app).get('/users')
@@ -24,18 +28,21 @@ describe("GET /users", () => {
     })
 })
 
+//test the GET /users/:id route.  This route should return a single user that was created by the beforeEach function which in this case is one user with a 200 status code if the id is valid and the user is found in the database table users.
 describe("GET /users/:id", () => {
     test("Gets a single user", async () => {
         const res = await request(app).get(`/users/${testUser.id}`)
         expect(res.statusCode).toBe(200);
         expect(res.body).toEqual({ user: testUser })
     })
+    //test the GET /users/:id route.  This route should return a 404 status code if the id is invalid.  
     test("Responds with 404 for invalid id", async () => {
         const res = await request(app).get(`/users/0`)
         expect(res.statusCode).toBe(404);
     })
 })
 
+//test the POST /users route.  This route should create a new user and return the new user with a 201 status code if the user is created successfully.
 describe("POST /users", () => {
     test("Creates a single user", async () => {
         const res = await request(app).post('/users').send({ name: 'BillyBob', type: 'staff' });
